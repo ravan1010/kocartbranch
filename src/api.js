304ec84adb3d56bc11f4909165e47c4e6771dc5a
ravan1010 <ravanravana177@gmail.com>
2026-03-06 12:34:09 +0530

    main

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://kocartserver.onrender.com',
+  // baseURL: 'https://kocartserver.onrender.com',
+    baseURL: 'https://api.kollegalakart.online',
   // baseURL: 'http://localhost:5001',
   withCredentials: true,
 });
--- src/pages/ownerdashboard.jsx
+++ src/pages/ownerdashboard.jsx
@@ -6,6 +6,7 @@ const Ownerdashboard = () => {
 
 
     const [otp, setotp] = useState()
+    const [open, setopen] = useState()
 
     // const [error, setError] = useState('');
     // const [success, setsuccess] = useState('');
@@ -24,6 +25,45 @@ const Ownerdashboard = () => {
         fetchOTP()
     },[])
 
+    const fetchDashboard = async () => {
+                try {
+                    const res = await api.get('/api/owner/dashboard')
+                    console.log(res.data.openORclose)
+                    setopen(res.data.openORclose)
+                } catch (error) {
+                    console.log(error)
+                }
+        }
+useEffect(() => {
+  const loadData = async () => {
+    await fetchDashboard()
+  }
+
+  loadData()
+}, [])
+
+    const handleToggle = async () => {
+
+    const newStatus = !open;
+    setopen(newStatus);
+
+    try {
+      await api.post(
+        "/api/owner/openORclose",
+        { open: newStatus },
+        { withCredentials: true }
+      ).then((res) => {
+        if (res.data.success === true) {
+          fetchDashboard()
+        }
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+
 
 
     return (
@@ -37,6 +77,31 @@ const Ownerdashboard = () => {
 
                     </div>
                 </div>
+
+                <div className="bg-white p-6 rounded-lg shadow">
+              <div className="flex items-center">
+                <div className="p-4 flex rounded-full bg-green-100 text-green-600 ">
+                  {open === false ? <>
+                    <DoorClosedLocked size={24} />
+                    <p className='mx-2'>close</p>
+                  </>
+                    :
+                    <>
+                      <DoorOpen size={24} />
+                      <p className='mx-2'>open</p>
+                    </>
+                  }
+                </div>
+                <div className="ml-8">
+                  <input
+                    type="checkbox"
+                    checked={open}
+                    onChange={handleToggle}
+                    className='w-6 h-6 border'
+                  />
+                </div>
+              </div>
+            </div>
 
                 {Array.isArray(otp) && otp.map((product) => (
                     <div key={product._id} className="border rounded-2xl shadow hover:shadow-lg transition duration-300 m-1 my-2">
