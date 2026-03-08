6dd840306f1a9ab79e0e6bcd7c64b1ccb99c2128
ravan1010 <ravanravana177@gmail.com>
2026-03-08 14:09:30 +0530

    main

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: 'https://kocartserver.onrender.com',
-    baseURL: 'https://api.kollegalakart.online',
+      baseURL: 'https://api.kocart.online/',
   // baseURL: 'http://localhost:5001',
   withCredentials: true,
 });
